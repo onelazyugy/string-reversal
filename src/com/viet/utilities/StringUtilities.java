235e00dc1b17beac13a9438d416f91c4b4b970ef
viet <viet_t_le@homedepot.com>
2016-12-04 20:49:40 -0500

    started replaceString method

--- src/com/viet/utilities/StringUtilities.java
+++ src/com/viet/utilities/StringUtilities.java
@@ -21,4 +21,15 @@ public final class StringUtilities {
         }while(nameLength != 0);
         return reversal;
     }
+
+    /**
+     * Give a string, another string to be replaced, and a replaced with string.
+     * Example: given 3 parameters (hello world, wo, **), it will generate the reseult with hello **rld
+     * @param toBeReplaced
+     * @return
+     */
+    public static String replaceString(String entireString, String toBeReplaced, String replacedWith){
+        //TODO
+        return "";
+    }
 }
